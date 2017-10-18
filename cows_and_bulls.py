7de97d3ee4c36ba963f06717a1f70f7016c38f91
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2017-10-18 22:05:07 +0200

    cows and bulls

---
+++ cows_and_bulls.py
@@ -0,0 +1,25 @@
+"""
+A program that plays the 'cows and bulls' game with the user. The game works like this:
+Randomly generate a 4-digit number. The user enters a 4-digit number. 
+For every digit that the user guessed correctly in the correct place, they have a 'cow'. 
+For every digit the user guessed correctly in the wrong place is a 'bull'. 
+Every time the user makes a guess, you tell them how many 'cows' and 'bulls' they have. 
+"""
+import random
+
+computer_input = str(random.randint(1000,9999))
+user_input = raw_input("Enter a 4-digit number: ")
+# test_input = raw_input("Enter another 4-digit number: ")
+
+def play_game(user, comp):
+  cows = 0
+  bulls = 0
+  for i, index in enumerate(user):
+    print comp
+    if user[i] == comp[i]:
+      cows +=1
+    elif user[i] in comp:
+      bulls +=1 
+  print "You have %s cows" % cows
+  print "You have %s bulls" % bulls
+play_game(user_input, computer_input) # remeber to change test to computer inpur
